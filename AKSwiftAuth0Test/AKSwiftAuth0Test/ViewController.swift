65b6a80599f8cb2164046a71137082f073d4ee05
Iuliia Zhelem <iuliia.zhelem@akvelon.com>
2016-06-17 19:19:34 +0300

    add new call for getting profile field "picture_large"

--- AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
+++ AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
@@ -11,6 +11,8 @@ import Lock
 import LockFacebook
 
 class ViewController: UIViewController {
+    
+    var token: A0Token?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,9 +24,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickGetBigPicture(sender: AnyObject) {
+        
+        if let actualToken = token {
+            
+            // We need url "https://<Auth0 Domain>/userinfo/?access_token=<access_token>"
+            let userDomain = (NSBundle.mainBundle().infoDictionary!["Auth0Domain"]) as! String
+            let urlString = "https://\(userDomain)/userinfo/?access_token=\(actualToken.accessToken!)"
+            let url = NSURL(string: urlString)
+            if let actualUrl = url {
+                let request = NSMutableURLRequest(URL: actualUrl)
+                request.HTTPMethod = "GET";
+            
+                NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {(data : NSData?, response : NSURLResponse?, error : NSError?) in
+                
+                    // Check if data was received successfully
+                    if error == nil && data != nil {
+                        do {
+                            // Convert NSData to Dictionary where keys are of type String, and values are of any type
+                            let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
+                            let urlForBigPicture = json["picture_large"] as! String
+                            self.showMessage("Big picture \(urlForBigPicture)");
+                        } catch {
+                            let dataString = String(data: data!, encoding: NSUTF8StringEncoding)
+                            print("Oops something went wrong: \(dataString)")
+                        }
+                    } else {
+                        print("Oops something went wrong: \(error)")
+                    }
+                }).resume()
+            } else {
+                self.showMessage("Incorrect url");
+            }
+        } else {
+            self.showMessage("Please login first");
+        }
+    }
+    
     @IBAction func clickFacebookLoginButton(sender: AnyObject) {
         let success = { (profile: A0UserProfile, token: A0Token) in
             print("We did it!. Logged in with Auth0.")
+            self.showMessage("We did it!. Logged in with Auth0.");
+            self.token = token;
         }
         let failure = { (error: NSError) in
             print("Oops something went wrong: \(error)")
@@ -32,6 +73,14 @@ class ViewController: UIViewController {
         let lock = A0Lock.sharedLock()
             lock.identityProviderAuthenticator().authenticateWithConnectionName("facebook", parameters: nil, success: success, failure: failure)
         
+    }
+    
+    func showMessage(message: String) {
+        dispatch_async(dispatch_get_main_queue()) {
+            let alert = UIAlertController(title: "Auth0", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
 }
 
